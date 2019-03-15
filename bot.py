f4209ce2740ac67b759f290ad3d285775581317c
Sampo <sampo.pelto@tuni.fi>
2019-03-15 10:49:36 +0200

    weather function is almost ready

--- bot.py
+++ bot.py
@@ -2,30 +2,55 @@
 # Copyright: Sampo Pelto 2019
 
 import logging
-import telegram
+from telegram import Bot
 from time import gmtime, strftime
 from configparser import ConfigParser
-
-cfg = ConfigParser()
-cfg.read('env.cfg')
+import pyowm
 
 
-def testsend(bot, msg):
-    chat_id = cfg['TELEGRAM']['id']
+def testsend(bot, msg, chat_id):
     bot.send_message(chat_id=chat_id, text=str(msg))
+
+
+def pvm():
+    return strftime("Tänään on %d.%m.", gmtime())
+
+
+def saa(owm):
+    observation = owm.weather_at_place('Tampere,FI')
+    w = observation.get_weather()
+    
+    condition = str(w).split('status=')[1][:-1].lower().split(",")[0]
+
+    temperature = str(int(w.get_temperature('celsius')['temp'])) + "°C"
+
+    return "Sää: " + condition + " ja " + temperature
+
+
+def nimi():
+    return "Nimipäivät: "
+
+
+def liputus():
+    return "Liputuspäivä: "
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger()
-    # logger.setLevel(logging.DEBUG)
     logger.setLevel(logging.ERROR)
 
-    bot = telegram.Bot(token=cfg['TELEGRAM']['token'])
+    cfg = ConfigParser()
+    cfg.read('env.cfg')
 
-    pvm = strftime("Tänään on %d.%m.", gmtime())
-    viesti = "--\n" + pvm + "\nNimipäivät: \nSää: \nLiputuspäivä: \n--"
-    testsend(bot, viesti)
+    bot = Bot(token=cfg['TELEGRAM']['token'])
+    chat_id = cfg['TELEGRAM']['id']
+    owm = pyowm.OWM(cfg['PYOWM']['token'])
+
+    viesti = pvm() + "\n" + nimi() + "\n" + \
+             saa(owm) + "\n" + liputus() + "\n--"
+
+    testsend(bot, viesti, chat_id)
     print(viesti)
 
 
