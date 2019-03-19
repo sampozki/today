13ced17918c8e3ee7d27201913df90b3f260dcea
Sampo <sampo.pelto@tuni.fi>
2019-03-19 23:36:18 +0200

    Added nimipäivät function

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from configparser import ConfigParser
 import pyowm
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 def testsend(bot, msg, chat_id):
@@ -23,10 +24,24 @@ def saa(owm):
     w = observation.get_weather()
 
     condition = str(w).split('status=')[1][:-1].lower().split(",")[0]
+    keli = condition
 
     temperature = str(int(w.get_temperature('celsius')['temp'])) + "°C"
 
-    return "Sää: " + condition + " ja " + temperature
+    if condition == "clouds":
+        keli = "Pilvistä"
+    elif condition == "fog":
+        keli = "Sumuista"
+    elif condition == "snow":
+        keli = "Lumista"
+    elif condition == "snow":
+        keli = "Vetistä"
+    elif condition == "clear":
+        keli = "Aurinkoista"
+    else:
+        keli = condition
+
+    return "Sää: " + keli + " ja " + temperature
 
 
 def nimi():
@@ -34,9 +49,16 @@ def nimi():
 
     soup = BeautifulSoup(requests.get(urli).content, "html.parser")
 
-    for i in soup.find('table'):
-        print(i)
-    return "Nimipäivät: "
+    kokonainen = str(soup.find('table'))
+    nimet = re.findall(r'>([a-zA-Z]*)<\/a>', kokonainen)
+    
+    if len(nimet) > 1:
+        return "Nimipäivät: " + ", ".join(nimet)
+    elif len(nimet) == 0:
+        return "Nimipäivä: " + ", ".join(nimet)
+    return "" 
+
+    
 
 
 def liputus():
@@ -56,7 +78,7 @@ def main():
     owm = pyowm.OWM(cfg['PYOWM']['token'])
 
     viesti = pvm() + "\n" + nimi() + "\n" + \
-             saa(owm) + "\n" + liputus() + "\n--"
+             saa(owm) + "\n" + liputus()
 
     testsend(bot, viesti, chat_id)
     print(viesti)
