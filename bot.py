185b0a4eeb43f2a2f9daee2bc36f1f0fc87ed8bf
Sampo <sampo.pelto@tuni.fi>
2019-03-21 18:11:27 +0200

    added daily fact from wikipedia

--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@
 
 from telegram import Bot
 from time import gmtime, strftime
+import datetime
 from configparser import ConfigParser
 import pyowm
 from bs4 import BeautifulSoup
+from random import choice
 import requests
 import re
 
@@ -40,7 +42,7 @@ def saa(owm):
     else:
         keli = condition
 
-    return f"Sää: {keli} ja {temperature}. "
+    return "Sää: {0} ja {1}. ".format(keli, temperature)
 
 
 def nimi():
@@ -53,7 +55,7 @@ def nimi():
     
     if len(nimet) > 1:
         return "Nimipäivät: " + ", ".join(nimet)
-    elif len(nimet) == 0:
+    elif len(nimet) == 1:
         return "Nimipäivä: " + ", ".join(nimet)
     return "Ei nimipäiviä!" 
 
@@ -64,20 +66,48 @@ def liputus():
 
     soup = BeautifulSoup(requests.get(urli).content, "html.parser")
 
-    return "Liputuspäivä: " + str(soup.find('h2')).split(">")[1].split("</")[0]
+    return str(soup.find('h2')).split(">")[1].split("</")[0]
+
+
+def fakta(d, m):
+    kuukausidict = {1: 'tammikuuta', 2: 'helmikuuta', 3: 'maaliskuuta', 4: 'huhtikuuta', 5: 'toukokuuta', 6: 'kesäkuuta', 7: 'heinäkuuta', 8: 'elokuuta', 9: 'syyskuuta', 10: 'lokakuuta', 11: 'marraskuuta', 12: 'joulukuuta'}
+
+    urli = "https://fi.m.wikipedia.org/wiki/" + str(d) + "._" + kuukausidict[m]
+
+    soup = BeautifulSoup(requests.get(urli).content, "html.parser")
+
+    lista = []
+    a = soup.prettify()
+    a = a.split("Tapahtumia")[3].split('id="Syn')[0]
+    a = a.replace("</li>", "??!!??")
+    a = re.sub('\s+', ' ', re.sub(r'<.*?>', "", a))
+    a = a.replace("<span", "").replace('"> Muokkaa', "")
+    
+    lista = a.split("??!!??")
+
+    return choice(lista)
 
 
 def main():
+
+    today = datetime.datetime.now()
 
     cfg = ConfigParser()
     cfg.read('env.cfg')
 
     bot = Bot(token=cfg['TELEGRAM']['token'])
-    chat_id = cfg['TELEGRAM']['id']
+
+    # TEStI
+    #chat_id = cfg['TELEGRAM']['id']
+    
+    # TUOTANTO
+    chat_id = cfg['TELEGRAM']['real']
+    
     owm = pyowm.OWM(cfg['PYOWM']['token'])
 
     viesti = pvm() + "\n" + saa(owm) + "\n" + \
-             nimi() + "\n" + liputus()
+             nimi() + "\n" + liputus() + "\n" + \
+             fakta(today.day, today.month)
 
     send(bot, viesti, chat_id)
     print(viesti)
